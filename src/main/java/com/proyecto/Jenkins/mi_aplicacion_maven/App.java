dc3029ed6f9f976e13e03ef64f39c5d755b751cb
consue.schuffe.t@gmail.com <consue.schuffe.t@gmail.com>
2025-04-14 11:56:55 -0400

    Inicializacion del proyecto Maven

---
+++ src/main/java/com/proyecto/Jenkins/mi_aplicacion_maven/App.java
@@ -0,0 +1,7 @@
+package com.proyecto.Jenkins.mi_aplicacion_maven;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/com/proyecto/Jenkins/mi_aplicacion_maven/AppTest.java
@@ -0,0 +1,13 @@
+package com.proyecto.Jenkins.mi_aplicacion_maven;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
